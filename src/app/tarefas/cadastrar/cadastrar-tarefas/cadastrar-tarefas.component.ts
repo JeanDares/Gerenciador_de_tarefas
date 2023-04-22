62df4ff46271888740fea7cdfd5e1b741b7c84e7
jean dares <jeanborgesdares@gmail.com>
2023-04-22 14:24:43 -0300

    implementando o cadastro de tarefas

--- src/app/tarefas/cadastrar/cadastrar-tarefas/cadastrar-tarefas.component.ts
+++ src/app/tarefas/cadastrar/cadastrar-tarefas/cadastrar-tarefas.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,  ViewChild } from '@angular/core';
+import { Tarefa } from '../../shared/tarefa.model';
+import { TarefaService } from '../../shared/tarefa.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar-tarefas',
@@ -6,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastrar-tarefas.component.css']
 })
 export class CadastrarTarefasComponent {
+
+  @ViewChild('formTarefa', { static: true }) formTarefa:NgForm
+
+  tarefa: Tarefa;
+
+  constructor(
+    private tarefaService: TarefaService,
+  	private router: Router) { }
+
+  ngOnInit() {
+  	this.tarefa = new Tarefa(0, "", false);
+  }
+
+  cadastrar(): void {
+    if (this.formTarefa.form.valid) {
+  	  this.tarefaService.cadastrar(this.tarefa);
+  	  this.router.navigate(["/tarefas/listar"]);
+    }
+  }
 
 }
--- src/app/tarefas/listar/listar-tarefas/listar-tarefas.component.ts
+++ src/app/tarefas/listar/listar-tarefas/listar-tarefas.component.ts
@@ -11,33 +11,31 @@ export class ListarTarefasComponent implements OnInit {
 
   tarefas: Tarefa[];
 
-  constructor ( private TarefaService: TarefaService)  {}
+  constructor(private tarefaService: TarefaService) {}
 
-  ngOnInit(): void {
-    this.tarefas = this.listarTodos()
+  ngOnInit() {
+  	this.tarefas = this.listarTodos();
   }
 
   listarTodos(): Tarefa[] {
-    return this.TarefaService.listarTodos()
-
+  	return this.tarefaService.listarTodos();
   }
 
   remover($event: any, tarefa: Tarefa): void {
     $event.preventDefault();
     if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"?')) {
-      this.TarefaService.remover(tarefa.id);
-      this.tarefas = this.TarefaService.listarTodos();
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
     }
   }
 
   alterarStatus(tarefa: Tarefa): void {
     if (confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '"?')) {
-      this.TarefaService.alterarStatus(tarefa.id);
-      this.tarefas = this.TarefaService.listarTodos();
+      this.tarefaService.alterarStatus(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
     }
   }
 
 }
-
 
 
